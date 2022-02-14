be25ab3bb807004a3f728499054b5d691b213804
Julieta <julietahurtadocabbad@gmail.com>
2022-02-14 15:18:23 -0300

    se agrego la configuracion de puerto y se creo una instancia de express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Prueba backend");
+import express from 'express';
+
+
+// configuracion de puerto
+const app = express();
+// crear una instancia de express
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'), () => {
+    console.log('Estoy en el puerto ' + app.get('port'));
+})
